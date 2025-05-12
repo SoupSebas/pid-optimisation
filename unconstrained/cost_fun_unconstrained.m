95ec4d261490e26e79575ff9078f8ff8d2affbb8
sebas <sebas@SEBA-WARE>
2025-05-12 09:41:59 +0200

    extended exception handling for when extreme cases have no margins at all

--- unconstrained/cost_fun_unconstrained.m
+++ unconstrained/cost_fun_unconstrained.m
@@ -5,13 +5,20 @@ function [f_BW, f_weighted, GM, PM, MM] = cost_fun_unconstrained(P,C)
 
     margins.DMFrequency = margins.DMFrequency./(2*pi);
     margins.GainMargin = 20*log10(margins.GainMargin);
-
-    GM = margins.GainMargin(1,end);
-    PM = margins.PhaseMargin(1,1);
-    freq = logspace(-2, 8, 1000); 
-    MM = 20*log10(max(abs(freqresp(S, freq))));
+    if ~isempty(margins.GainMargin) && ~isempty(margins.PhaseMargin) && ~isempty(margins.DMFrequency)
+        GM = margins.GainMargin(1,end);
+        PM = margins.PhaseMargin(1,1);
+        freq = logspace(-2, 8, 1000); 
+        MM = 20*log10(max(abs(freqresp(S, freq))));
+        f_BW = margins.DMFrequency(1,1);
+    else
+        GM = 3;
+        PM = 0;
+        MM = 7;
+        f_BW = 0;
+    end
     
-    f_BW = margins.DMFrequency(1,1);
+    
     penalty = barrier_function(GM,PM,MM,100);
     f_weighted = f_BW - penalty;
     fprintf(['Bandwidth: %.0f | Penalty: %.0f | ' ...
